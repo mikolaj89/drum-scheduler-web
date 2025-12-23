4145dde483e96ffc6cd8d4da83b3466a81d41412
Mikolaj Wroblewski <mikolaj9891@gmail.com>
2025-12-23 16:58:29 +0100

    # add @packages/contracts for exposed API types

---
+++ packages/contracts/src/types.ts
@@ -0,0 +1,13 @@
+import { categoriesSchema, exercisesSchema, sessionexercisesSchema, sessionsSchema } from "@drum-scheduler/api/db/schema";
+
+export type Category = typeof categoriesSchema.$inferSelect;
+export type CategoryInput = typeof categoriesSchema.$inferInsert;
+
+export type Exercise = typeof exercisesSchema.$inferSelect;
+export type ExerciseInput = typeof exercisesSchema.$inferInsert;
+
+export type SessionExercise = typeof sessionexercisesSchema.$inferSelect;
+export type SessionExerciseInput = typeof sessionexercisesSchema.$inferInsert;
+
+export type Session = typeof sessionsSchema.$inferSelect;
+export type SessionInput = typeof sessionsSchema.$inferInsert;
