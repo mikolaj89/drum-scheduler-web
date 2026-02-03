782d34641321e6053267a8bf765bf97ab604de88
Mikolaj Wroblewski <mikolaj9891@gmail.com>
2026-02-03 11:19:26 +0100

    feature(mobile) - use paperTheme style

--- apps/drum-scheduler-app/components/layout/screen-container/screen-container.tsx
+++ apps/drum-scheduler-app/components/layout/screen-container/screen-container.tsx
@@ -2,12 +2,27 @@ import React, { ReactNode } from 'react';
 import { View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { styles } from './screen-container.style';
-import { PaperProvider, useTheme } from 'react-native-paper';
+import { MD3LightTheme as PaperTheme, PaperProvider, useTheme } from 'react-native-paper';
+
+const paperTheme = {
+  ...PaperTheme,
+  roundness: 3,
+  //setup if needed
+  // colors: {
+  //   ...PaperTheme.colors,
+  //   background: appTheme.colors.bg,
+  //   surface: appTheme.colors.surface,
+  //   primary: appTheme.colors.primary,
+  //   outline: appTheme.colors.border,
+  //   onSurface: appTheme.colors.text,
+  //   onSurfaceVariant: appTheme.colors.textMuted,
+  // },
+};
 
 export const ScreenContainer = ({ children }: { children: ReactNode }) => {
   const theme = useTheme();
   return (
-    <PaperProvider>
+    <PaperProvider theme={paperTheme}>
       <SafeAreaView style={styles.safe}>
         <View
           style={[styles.screen, { backgroundColor: theme.colors.background }]}
